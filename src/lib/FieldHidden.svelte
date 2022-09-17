534c1d4c916644c640a14a69a40f7d643a5804ce
Nick Wing <nwing@txstate.edu>
2022-09-16 21:45:28 -0500

    add FieldHidden for easily adding contextual data to a form, e.g. if the user is creating data "inside" another object, you may need to inject the parent object id into the form

---
+++ src/lib/FieldHidden.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { Field, nullableSerialize, nullableDeserialize, FormStore, FORM_CONTEXT } from '@txstate-mws/svelte-forms'
+  import { getContext } from 'svelte'
+  export let id: string | undefined = undefined
+  export let path: string
+  export let value = ''
+  export let notNull = false
+  export let conditional: boolean|undefined = undefined
+  const store = getContext<FormStore>(FORM_CONTEXT)
+  $: store.setField(path, value)
+</script>
+
+<Field {path} {conditional} serialize={!notNull && nullableSerialize} deserialize={!notNull && nullableDeserialize} let:value>
+  <input type="hidden" name={path} {value} {id}>
+</Field>
