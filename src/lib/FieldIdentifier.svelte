3d37857b807f73b53ea4098645017691c9f22f5f
Nick Wing <nwing@txstate.edu>
2022-10-07 21:26:22 -0500

    add a FieldIdentifier field for generating random identifier strings for components and then keeping them intact on edits so they can be used long-term

---
+++ src/lib/FieldIdentifier.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { Field, nullableSerialize, nullableDeserialize } from '@txstate-mws/svelte-forms'
+  import { randomid } from 'txstate-utils'
+
+  export let path: string
+  export let conditional: boolean|undefined = undefined
+</script>
+
+<Field {path} {conditional} defaultValue={randomid()} serialize={nullableSerialize} deserialize={nullableDeserialize} let:value>
+  <input type="hidden" name={path} {value}>
+</Field>
