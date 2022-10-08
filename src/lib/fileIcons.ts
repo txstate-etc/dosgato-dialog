cc4d204045e2aa0e468645efaa441a17f3d04731
Nick Wing <nwing@txstate.edu>
2022-10-07 21:26:14 -0500

    change file-type icons to phosphor

--- src/lib/fileIcons.ts
+++ src/lib/fileIcons.ts
@@ -1,50 +1,50 @@
 import type { IconifyIcon } from '@iconify/svelte'
-import archiveOutline from '@iconify-icons/mdi/archive-outline.js'
-import fileCodeOutline from '@iconify-icons/mdi/file-code-outline.js'
-import fileDocumentOutline from '@iconify-icons/mdi/file-document-outline.js'
-import fileMusicOutline from '@iconify-icons/mdi/file-music-outline.js'
-import fileOutline from '@iconify-icons/mdi/file-outline.js'
-import fileTableOutline from '@iconify-icons/mdi/file-table-outline.js'
-import fileVideoOutline from '@iconify-icons/mdi/file-video-outline.js'
-import fileImageOutline from '@iconify-icons/mdi/file-image-outline.js'
-import microsoftExcel from '@iconify-icons/mdi/microsoft-excel.js'
-import microsoftWord from '@iconify-icons/mdi/microsoft-word.js'
-import noteTextOutline from '@iconify-icons/mdi/note-text-outline.js'
+import archiveLight from '@iconify-icons/ph/archive-light.js'
+import fileCodeLight from '@iconify-icons/ph/file-code-light.js'
+import fileTextLight from '@iconify-icons/ph/file-text-light.js'
+import fileAudioLight from '@iconify-icons/ph/file-audio-light.js'
+import filePdfLight from '@iconify-icons/ph/file-pdf-light'
+import fileLight from '@iconify-icons/ph/file-light.js'
+import fileCsvLight from '@iconify-icons/ph/file-csv-light.js'
+import fileVideoLight from '@iconify-icons/ph/file-video-light.js'
+import fileImageLight from '@iconify-icons/ph/file-image-light.js'
+import microsoftExcel from '@iconify-icons/ph/file-xls-light.js'
+import microsoftWord from '@iconify-icons/ph/file-doc-light.js'
 
 const fileIconsByMime: Record<string, IconifyIcon> = {
   'application/vnd.ms-excel': microsoftExcel,
   'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': microsoftExcel,
   'application/vnd.oasis.opendocument.spreadsheet': microsoftExcel,
-  'text/csv': fileTableOutline,
+  'text/csv': fileCsvLight,
   'application/msword': microsoftWord,
   'application/vnd.openxmlformats-officedocument.wordprocessingml.document': microsoftWord,
   'application/vnd.oasis.opendocument.text': microsoftWord,
   'application/rtf': microsoftWord,
-  'application/pdf': fileDocumentOutline,
-  'application/json': fileCodeOutline,
-  'application/x-httpd-php': fileCodeOutline,
-  'application/x-sh': fileCodeOutline,
-  'application/xml': fileCodeOutline,
-  'text/css': fileCodeOutline,
-  'text/html': fileCodeOutline,
-  'text/javascript': fileCodeOutline,
-  'application/gzip': archiveOutline,
-  'application/java-archive': archiveOutline,
-  'application/zip': archiveOutline,
-  'application/vnd.apple.installer+xml': archiveOutline,
-  'application/vnd.rar': archiveOutline,
-  'application/x-7z-compressed': archiveOutline,
-  'application/x-bzip': archiveOutline,
-  'application/x-bzip2': archiveOutline,
-  'application/x-tar': archiveOutline
+  'application/pdf': filePdfLight,
+  'application/json': fileCodeLight,
+  'application/x-httpd-php': fileCodeLight,
+  'application/x-sh': fileCodeLight,
+  'application/xml': fileCodeLight,
+  'text/css': fileCodeLight,
+  'text/html': fileCodeLight,
+  'text/javascript': fileCodeLight,
+  'application/gzip': archiveLight,
+  'application/java-archive': archiveLight,
+  'application/zip': archiveLight,
+  'application/vnd.apple.installer+xml': archiveLight,
+  'application/vnd.rar': archiveLight,
+  'application/x-7z-compressed': archiveLight,
+  'application/x-bzip': archiveLight,
+  'application/x-bzip2': archiveLight,
+  'application/x-tar': archiveLight
 }
 const prefixes: Record<string, IconifyIcon> = {
-  image: fileImageOutline,
-  text: noteTextOutline,
-  video: fileVideoOutline,
-  audio: fileMusicOutline
+  image: fileImageLight,
+  text: fileTextLight,
+  video: fileVideoLight,
+  audio: fileAudioLight
 }
 
 export function iconForMime (mime: string) {
-  return fileIconsByMime[mime] ?? prefixes[mime.split('/', 2)[0]] ?? fileOutline
+  return fileIconsByMime[mime] ?? prefixes[mime.split('/', 2)[0]] ?? fileLight
 }
