ba36179abd2b8f9b1dd35109b800e1691fb94ae2
Anne Faber <anne.faber@txstate.edu>
2023-02-22 13:58:59 -0600

    Allow booleans and numbers in hidden field

--- src/lib/FieldHidden.svelte
+++ src/lib/FieldHidden.svelte
@@ -4,8 +4,10 @@
   import { isNotBlank } from 'txstate-utils'
   export let id: string | undefined = undefined
   export let path: string
-  export let value = ''
+  export let value: string | boolean | number = ''
   export let notNull = false
+  export let boolean = false
+  export let number = false
   export let conditional: boolean|undefined = undefined
   const store = getContext<FormStore>(FORM_CONTEXT)
   const inheritedPath = getContext<string>(FORM_INHERITED_PATH)
@@ -13,6 +15,6 @@
   $: store.setField(finalPath, value)
 </script>
 
-<Field {path} {conditional} serialize={!notNull ? nullableSerialize : undefined} deserialize={!notNull ? nullableDeserialize : undefined} let:value>
+<Field {path} {conditional} {boolean} {number} serialize={!notNull ? nullableSerialize : undefined} deserialize={!notNull ? nullableDeserialize : undefined} let:value>
   <input type="hidden" name={path} {value} {id}>
 </Field>
