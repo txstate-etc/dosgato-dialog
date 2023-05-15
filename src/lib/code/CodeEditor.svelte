59cbb13561b0ce7d61c4c7dd3ee456bc4565f810
Nick Wing <nwing@txstate.edu>
2023-05-14 21:34:35 -0500

    finish moving code editor to codemirror

---
+++ src/lib/code/CodeEditor.svelte
@@ -0,0 +1,82 @@
+<script lang="ts">
+  import { type HTMLActionEntry, passActions } from '@txstate-mws/svelte-components'
+  import type { EditorView, ViewUpdate } from '@codemirror/view'
+  import { createEventDispatcher, onDestroy, onMount } from 'svelte'
+  import { getDescribedBy } from '$lib'
+  let className = ''
+  export { className as class }
+  export let id: string | undefined = undefined
+  export let rows = 8
+  export let language: 'js'|'css'|'html'
+  export let use: HTMLActionEntry[] = []
+  export let inputelement: HTMLElement | undefined = undefined
+  export let extradescid: string | undefined = undefined
+  export let helptextid: string | undefined = undefined
+  export let messagesid: string | undefined = undefined
+  export let valid: boolean | undefined = undefined
+  export let invalid: boolean | undefined = undefined
+  export let value: any
+
+  const dispatch = createEventDispatcher()
+
+  let updateCode: (code: string) => void
+  let editorelement: HTMLElement | undefined
+  let editor: EditorView
+  onMount(async () => {
+    const { EditorView, minimalSetup } = await import('codemirror')
+    const { indentWithTab } = await import('@codemirror/commands')
+    const { lineNumbers, highlightActiveLine, highlightActiveLineGutter, keymap } = await import('@codemirror/view')
+    const { langmap } = await import('./langs.js')
+    editor = new EditorView({
+      extensions: [
+        minimalSetup,
+        lineNumbers(),
+        highlightActiveLine(),
+        highlightActiveLineGutter(),
+        langmap[language],
+        keymap.of([indentWithTab]),
+        EditorView.updateListener.of((v: ViewUpdate) => {
+          if (v.docChanged) {
+            const newval = editor.state.doc.toString()
+            if (value !== newval) dispatch('change', newval)
+          }
+        })
+      ],
+      parent: editorelement
+    })
+    updateCode = code => {
+      if (editor.state.doc.toString() !== code) editor.update([editor.state.update({ changes: { from: 0, to: editor.state.doc.length, insert: code } })])
+    }
+    inputelement = editorelement?.querySelector('.cm-content') ?? undefined
+    if (id) inputelement?.setAttribute('id', id)
+    if (className) inputelement?.classList.add(...className.split(' '), ...[])
+    updateValidState()
+  })
+  onDestroy(() => {
+    editor?.destroy()
+  })
+  $: updateCode?.(value ?? '')
+  function updateValidState (..._: any[]) {
+    inputelement?.setAttribute('aria-invalid', String(!!invalid))
+    const descby = getDescribedBy([messagesid, helptextid, extradescid])
+    if (descby) inputelement?.setAttribute('aria-describedby', descby)
+    else inputelement?.removeAttribute('aria-describedby')
+  }
+  $: updateValidState(invalid, messagesid)
+</script>
+
+<div bind:this={editorelement} style:--cm-editor-minh="{rows * 1.5}em" class:valid class:invalid on:paste on:focusout use:passActions={use}></div>
+
+<style>
+  div {
+    background-color: white;
+  }
+  div :global(.cm-content), div :global(.cm-gutter) {
+    min-height: var(--cm-editor-minh, 10em);
+  }
+
+  div :global(.cm-scroller) {
+    overflow: auto;
+    resize: vertical;
+  }
+</style>
--- src/lib/code/FieldCodeEditor.svelte
+++ src/lib/code/FieldCodeEditor.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
-  import { type HTMLActionEntry, passActions } from '@txstate-mws/svelte-components'
-  import { nullableSerialize, nullableDeserialize, FORM_CONTEXT, FormStore, FORM_INHERITED_PATH } from '@txstate-mws/svelte-forms'
-  import type { ViewUpdate } from '@codemirror/view'
-  import { getContext, onMount } from 'svelte'
-  import { isNotBlank } from 'txstate-utils'
-  import { getDescribedBy, FieldStandard } from '$lib'
+  import type { HTMLActionEntry } from '@txstate-mws/svelte-components'
+  import { nullableSerialize, nullableDeserialize } from '@txstate-mws/svelte-forms'
+  import { FieldStandard } from '$lib'
+  import CodeEditor from './CodeEditor.svelte'
   let className = ''
   export { className as class }
   export let id: string | undefined = undefined
@@ -16,84 +14,13 @@
   export let conditional: boolean|undefined = undefined
   export let required = false
   export let use: HTMLActionEntry[] = []
-  export let inputelement: HTMLTextAreaElement = undefined as any
+  export let inputelement: HTMLElement = undefined as any
   export let related: true | number = 0
   export let extradescid: string | undefined = undefined
   export let helptext: string | undefined = undefined
   export let language: 'js'|'css'|'html'
-
-  const store = getContext<FormStore>(FORM_CONTEXT)
-  const inheritedPath = getContext<string>(FORM_INHERITED_PATH)
-  const finalPath = [inheritedPath, path].filter(isNotBlank).join('.')
-  const value = store.getField<string | undefined>(finalPath)
-
-  let editorelement: HTMLElement
-  let updateCode: (code: string) => void
-  onMount(async () => {
-    const { EditorView, minimalSetup } = await import('codemirror')
-    const { indentWithTab } = await import('@codemirror/commands')
-    const { lineNumbers, highlightActiveLine, highlightActiveLineGutter, keymap } = await import('@codemirror/view')
-    const { langmap } = await import('./langs.js')
-    const editor = new EditorView({
-      extensions: [
-        minimalSetup,
-        lineNumbers(),
-        highlightActiveLine(),
-        highlightActiveLineGutter(),
-        langmap[language],
-        keymap.of([indentWithTab]),
-        EditorView.updateListener.of((v: ViewUpdate) => {
-          if (v.docChanged) {
-            const newval = editor.state.doc.toString()
-            if ($value !== newval) mySetVal?.(newval)
-          }
-        })
-      ],
-      parent: editorelement
-    })
-    updateCode = code => {
-      if (editor.state.doc.toString() !== code) editor.update([editor.state.update({ changes: { from: 0, to: editor.state.doc.length, insert: code } })])
-    }
-    inputelement = editorelement.querySelector(`#${id}`) as HTMLTextAreaElement
-    if (className) inputelement.classList.add(...className.split(' '))
-    updateValidState(invalid, messagesid)
-  })
-  $: updateCode?.($value ?? '')
-
-  let invalid: boolean
-  let messagesid: string | undefined
-  let helptextid: string | undefined
-  let mySetVal: (code?: any) => void
-  function setSlotProps (helptextidIn: string | undefined, setValIn: any) {
-    helptextid = helptextidIn
-    mySetVal = setValIn
-  }
-  function updateValidState (invalidIn: boolean | undefined, messagesIdIn: any) {
-    invalid = !!invalidIn
-    messagesid = messagesIdIn
-    inputelement?.setAttribute('aria-invalid', String(!!invalid))
-    const descby = getDescribedBy([messagesid, helptextid, extradescid])
-    if (descby) inputelement?.setAttribute('aria-describedby', descby)
-    else inputelement?.removeAttribute('aria-describedby')
-  }
 </script>
 
 <FieldStandard bind:id {label} {path} {required} {defaultValue} {conditional} {related} {helptext} serialize={!notNull ? nullableSerialize : undefined} deserialize={!notNull ? nullableDeserialize : undefined} let:value let:valid let:invalid let:id let:onBlur let:setVal let:messagesid let:helptextid>
-  {@const _ = setSlotProps(helptextid, setVal)}
-  {@const __ = updateValidState(invalid, messagesid)}
-  <div bind:this={editorelement} style:--cm-editor-minh="{rows * 1.5}em" class:valid class:invalid on:paste on:focusout={onBlur} use:passActions={use}></div>
+  <CodeEditor {id} bind:inputelement {language} {rows} class={className} {value} {valid} {invalid} {helptextid} {messagesid} {extradescid} on:paste on:focusout={onBlur} {use} on:change={e => setVal(e.detail)}/>
 </FieldStandard>
-
-<style>
-  div {
-    background-color: white;
-  }
-  div :global(.cm-content), div :global(.cm-gutter) {
-    min-height: var(--cm-editor-minh, 10em);
-  }
-
-  div :global(.cm-scroller) {
-    overflow: auto;
-    resize: vertical;
-  }
-</style>
