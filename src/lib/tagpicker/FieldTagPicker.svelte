ab84a813527156fde5241516e70a1c82200ea876
Nick Wing <nwing@txstate.edu>
2024-08-23 14:31:49 -0500

    provide lookupByValue to the tagpicker, release 1.2.1

--- src/lib/tagpicker/FieldTagPicker.svelte
+++ src/lib/tagpicker/FieldTagPicker.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
   import FieldMultiselect from '$lib/FieldMultiselect.svelte'
   import { getContext } from 'svelte'
+  import { isNotBlank } from 'txstate-utils'
   import { TAG_API_CONTEXT, type Tag, type TagClient, type TagGroup } from './TagAPI'
-    import { isNotBlank } from 'txstate-utils';
 
   export let path: string
   export let label: string
@@ -13,15 +13,30 @@
   let lasttarget: any = -1
   let groups: TagGroup[] = []
   let tags: (Tag & { lcname: string, group: TagGroup & { lctitle: string } })[]
-  async function getOptions (search: string) {
+  async function fetchAvailableTags () {
     if (lasttarget !== target) {
       groups = await tagClient.availableForTarget(target)
       tags = groups.flatMap(g => g.tags.map(t => ({ ...t, lcname: t.name.toLocaleLowerCase(), group: { ...g, lctitle: g.title?.toLocaleLowerCase() ?? '' } })))
       lasttarget = target
     }
+  }
+
+  function tagToOption (tag: (typeof tags)[0]) {
+    return { label: `${tag.group.title ?? ''}${isNotBlank(tag.group.title) ? ': ' : ''}${tag.name}`, value: tag.id }
+  }
+
+  async function getOptions (search: string) {
+    await fetchAvailableTags()
     const lcsearch = search?.toLocaleLowerCase() ?? ''
-    return tags.filter(t => t.lcname.includes(lcsearch) || t.group.lctitle.includes(lcsearch)).map(t => ({ label: `${t.group.title ?? ''}${isNotBlank(t.group.title) ? ': ' : ''}${t.name}`, value: t.id }))
+    return tags.filter(t => t.lcname.includes(lcsearch) || t.group.lctitle.includes(lcsearch)).map(tagToOption)
+  }
+
+  async function lookupByValue (id: string) {
+    await fetchAvailableTags()
+    const tag = tags.find(t => t.id === id)
+    if (!tag) return undefined
+    return tagToOption(tag)
   }
 </script>
 
-<FieldMultiselect {path} {label} {getOptions} />
+<FieldMultiselect {path} {label} {getOptions} {lookupByValue} />
