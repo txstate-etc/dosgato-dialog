fbba94bb946d20cdc12e0c34706c7740ac3600b7
Anne Faber <anne.faber@txstate.edu>
2025-03-13 09:09:06 -0500

    Fix invalid date handling in FieldDateTime. Don't set the value until both date and time have been entered

--- src/lib/FieldDateTime.svelte
+++ src/lib/FieldDateTime.svelte
@@ -2,6 +2,7 @@
   import { datetimeSerialize, datetimeDeserialize } from '@txstate-mws/svelte-forms'
   import FieldStandard from './FieldStandard.svelte'
   import Input from './Input.svelte'
+  import { createEventDispatcher } from 'svelte'
   let className = ''
   export { className as class }
   export let id: string | undefined = undefined
@@ -17,23 +18,27 @@
   export let extradescid: string | undefined = undefined
   export let helptext: string | undefined = undefined
   export let inputelement: HTMLInputElement = undefined!
-  let showBadInputMessage = false
 
-  function wrapOnBlur (onBlur) {
-    const date = new Date(inputelement.value)
-    showBadInputMessage = inputelement.validity.badInput || !(date instanceof Date && !isNaN(date.valueOf()))
-    onBlur()
+  const dispatch = createEventDispatcher()
+
+  function onChange (setVal) {
+    return () => {
+      if (inputelement) {
+        const date = new Date(inputelement.value)
+        const invalid = inputelement.validity.badInput || !(date instanceof Date && !isNaN(date.valueOf()))
+        if (invalid) {
+          setVal(undefined)
+          dispatch('change', undefined)
+        } else {
+          setVal(datetimeDeserialize(inputelement.value))
+          dispatch('change', datetimeDeserialize(inputelement.value))
+        }
+      }
+    }
   }
 </script>
 
-<FieldStandard bind:id {label} {path} {required} {defaultValue} {conditional} {related} {helptext} serialize={datetimeSerialize} deserialize={datetimeDeserialize} let:value let:valid let:invalid let:id let:onBlur let:onChange let:helptextid let:messagesid>
-  {#if showBadInputMessage}<div class="bad-input-warning" aria-live='polite'>{`Field ${label}`} must include both a date and time</div>{/if}
-  <Input bind:inputelement={inputelement} type="datetime-local" name={path} {value} {id} class="dialog-input {className}" onBlur={() => { wrapOnBlur(onBlur) }} {onChange} {valid} {invalid} {min} {max} {step} {extradescid} {messagesid} {helptextid}/>
+<FieldStandard bind:id {label} {path} {required} {defaultValue} {conditional} {related} {helptext} serialize={datetimeSerialize} deserialize={datetimeDeserialize} let:value let:valid let:invalid let:id let:onBlur let:helptextid let:messagesid let:setVal>
+  <Input bind:inputelement={inputelement} type="datetime-local" name={path} {value} {id} class="dialog-input {className}" {onBlur} onChange={onChange(setVal)} {valid} {invalid} {min} {max} {step} {extradescid} {messagesid} {helptextid}/>
 </FieldStandard>
 
-<style>
-  .bad-input-warning {
-    margin-bottom: 0.3em;
-    color: var(--dg-danger-bg, #9a3332);
-  }
-</style>
