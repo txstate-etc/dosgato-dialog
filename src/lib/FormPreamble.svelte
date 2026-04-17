fee216e57302f12de49b01fb1530f9c4c6c8c4e2
Nick Wing <nwing@txstate.edu>
2026-04-17 16:43:31 -0500

    added a FormPreamble component for adding explanations to the top of FormDialogs, release 1.6.5

---
+++ src/lib/FormPreamble.svelte
@@ -0,0 +1,11 @@
+<div class="dialog-form-preamble">
+  <slot />
+</div>
+
+<style>
+  .dialog-form-preamble {
+    margin-top: 1em;
+    color: var(--dialog-muted-text, #595959);
+    font-size: 0.9em;
+  }
+</style>
